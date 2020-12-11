26fd79bca33d8eec2a9752bd6a31825f5b141e05
Gabe-Corral <78gabe.corral@gmail.com>
2020-12-11 14:27:23 -0600

    added webbrowser feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import feedparser, os, webbrowser, curses
 
-
 class RssFeed:
 
     def __init__(self):
@@ -10,6 +9,9 @@ class RssFeed:
         self.current_row = 0
         self.current_video = 0
         self.video_count = 0
+        self.showVideo = False
+        self.videoName = ""
+        self.key = ""
 
         with open('urls.txt', 'r') as urls:
             for line in urls:
@@ -30,8 +32,12 @@ class RssFeed:
         fullFeed = {newFeed['feed']['title']: feedData}
         self.rssFeeds.append(fullFeed)
 
+
     def printFeeds(self, stdscr):
         stdscr.clear()
+        curses.curs_set(0)
+        curses.use_default_colors()
+        curses.init_pair(1, curses.COLOR_RED, -1)
         h, w = stdscr.getmaxyx()
         for idx, i in enumerate(self.rssFeeds):
             key_name = str([key for key in i]).strip("'[]'")
@@ -45,11 +51,10 @@ class RssFeed:
                 stdscr.addstr(y, x, key_name)
         stdscr.refresh()
 
+
     def main(self, stdscr):
         self.second_loop = False
         curses.curs_set(0)
-        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
-        #current_row = 0
         self.printFeeds(stdscr)
 
         while self.main_loop:
@@ -62,21 +67,24 @@ class RssFeed:
             elif key == curses.KEY_ENTER or key in [10, 13]:
                 self.showChannel(stdscr)
                 self.main_loop = False
-                stdscr.refresh()
-                stdscr.getch()
 
             self.printFeeds(stdscr)
             stdscr.refresh()
 
+
     def printVideos(self, stdscr):
         stdscr.clear()
+        curses.init_pair(1, curses.COLOR_RED, -1)
         h, w = stdscr.getmaxyx()
         for k, v in self.rssFeeds[self.current_row].items():
             for idx, i in enumerate(self.rssFeeds[self.current_row][k]):
                 self.video_count += 1
                 x = w//2 - len(i)//2
                 y = h//2 - 15//2 + idx
-                if idx == self.current_video:
+                if self.showVideo == True:
+                    self.showVideoDetails(stdscr, i, k)
+                    break
+                elif idx == self.current_video:
                     stdscr.attron(curses.color_pair(1))
                     stdscr.addstr(y, x, i)
                     stdscr.attroff(curses.color_pair(1))
@@ -85,8 +93,8 @@ class RssFeed:
         stdscr.refresh()
 
 
-
     def showChannel(self, stdscr):
+        self.showVideo = False
         self.second_loop = True
         curses.curs_set(0)
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
@@ -100,7 +108,7 @@ class RssFeed:
             elif key == curses.KEY_DOWN and self.current_video < self.video_count:
                 self.current_video += 1
             elif key == curses.KEY_ENTER or key in [10, 13]:
-                print("enter")
+                self.showVideo = True
             elif key == curses.KEY_LEFT:
                 self.main_loop = True
                 self.main(stdscr)
@@ -108,6 +116,37 @@ class RssFeed:
             stdscr.refresh()
 
 
+    def printVideoDetails(self, stdscr, videoName, key):
+        # self.rssFeeds[self.current_row][key][videoName][2]
+        details = ['Title:', videoName, 'URL:',
+        self.rssFeeds[self.current_row][key][videoName][1],
+        'Author:', self.rssFeeds[self.current_row][key][videoName][3],
+        'Channel:', self.rssFeeds[self.current_row][key][videoName][4]
+        ]
+        h, w = stdscr.getmaxyx()
+        for idx, i in enumerate(details):
+            x = w//2 - len(i)//2
+            y = h//2 - len(details)//2 + idx
+            stdscr.addstr(y, x, i)
+        stdscr.refresh()
+
+
+    def showVideoDetails(self, stdscr, videoName, key):
+        self.second_loop = False
+        self.main_loop = False
+        self.key = key
+        self.videoName = videoName
+        self.printVideoDetails(stdscr, self.videoName, self.key)
+
+        while self.showVideo:
+            key = stdscr.getch()
+            if key == curses.KEY_ENTER or key in [10, 13]:
+                webbrowser.open(self.rssFeeds[self.current_row][self.key][self.videoName][1])
+            elif key == curses.KEY_LEFT:
+                self.second_loop = True
+                self.showChannel(stdscr)
+            self.printVideoDetails(stdscr, self.videoName, self.key)
+            stdscr.refresh()
 
 
 if __name__=='__main__':
